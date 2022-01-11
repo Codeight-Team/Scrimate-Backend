041ec0dbda62baeb5004bf49b0828b2675c5a46a
Rafiariyanta <rafiariyanta08@gmail.com>
2022-01-11 21:53:30 +0700

    Fix time expiration

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     secret: "scrimate-secret-key",
-    jwtExpiration: 60, // 1 minutes
-    jwtRefreshExpiration: 120 //2 minutes
+    jwtExpiration: 86400, // 1 Day
+    jwtRefreshExpiration: 604800 //1 week
   };
